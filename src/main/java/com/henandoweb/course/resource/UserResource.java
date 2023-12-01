f225b95016941dde6168da91c11483cb3186ebd7
Hernando-s <hernando.sousa13@gmail.com>
2023-12-01 13:04:11 -0300

    testando endpoint

---
+++ src/main/java/com/henandoweb/course/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.henandoweb.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.henandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Hernando", "hernando@gmail.com", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
